ee87637bac247acf2ad6cdc2c69d958ed18933b8
netbek932 <rjeren22@gmail.com>
2022-08-17 22:33:06 -0400

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,18 @@
+const models = require('../models/model');
+
 const parseCookies = (req, res, next) => {
+
+  var cookies = req.headers.cookie;
+  if (cookies) {
+    var cookieArr = cookies.split('; ');
+    for (var i = 0; i < cookieArr.length; i++) {
+      var arr = cookieArr[i].split('=');
+      console.log('arr', arr);
+      req.cookies[arr[0]] = arr[1];
+    }
+  }
+  next();
+
 };
 
 module.exports = parseCookies;
